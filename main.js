29a15ec361859e67cea573b6e17e9439ec17cdee
BekzodQodirovdev <oldakkaun4@gmail.com>
2024-10-23 01:53:56 +0500

    js end

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target && i != j) {
-        return [i, j];
-      }
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  function getNum(n) {
+    let res = "";
+    while (n !== null) {
+      res = n.val + res;
+      n = n.next;
     }
+    return BigInt(res);
   }
+
+  let n = getNum(l1);
+  let m = getNum(l2);
+  let resSum = n + m;
+
+  let resStr = resSum.toString();
+  let res = new ListNode(parseInt(resStr[resStr.length - 1]));
+  let current = res;
+
+  for (let i = resStr.length - 2; i >= 0; i--) {
+    current.next = new ListNode(parseInt(resStr[i]));
+    current = current.next;
+  }
+
+  return res;
 };

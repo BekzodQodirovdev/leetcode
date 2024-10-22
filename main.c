2df835dc20cc4f2da1c0c7d0af73022eaea33c53
BekzodQodirovdev <oldakkaun4@gmail.com>
2024-10-23 01:47:31 +0500

    end

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 
-int main(){
-    printf("Hello world!\n");
+    int* res = (int*)malloc(2 * sizeof(int));  
+    *returnSize = 0;
+    for(int i = 0; i < numsSize; i++){
+        for(int j = i + 1; j < numsSize; j++){
+            if(nums[i] + nums[j] == target){
+                res[0] = i;
+                res[1] = j;
+                *returnSize = 2;
+                return res;
+            }
+        }
+    }
+    free(res);
+    // return NULL;
 }
